9ddfcb154dbc7c938dd1ba9fcde926fe65a72f0b
Isuru Ekanayaka <133802550+isurulekanayaka@users.noreply.github.com>
2024-12-27 05:23:16 +0530

    enable right click event

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -7,19 +7,19 @@ import "slick-carousel/slick/slick-theme.css";
 const Layouts = ({ children }) => {
   
   // Disable right-click functionality
-  useEffect(() => {
-    const disableRightClick = (event) => {
-      event.preventDefault();
-    };
+  // useEffect(() => {
+  //   const disableRightClick = (event) => {
+  //     event.preventDefault();
+  //   };
 
-    // Attach the event listener on mount
-    document.addEventListener('contextmenu', disableRightClick);
+  //   // Attach the event listener on mount
+  //   document.addEventListener('contextmenu', disableRightClick);
 
-    // Cleanup the event listener on unmount
-    return () => {
-      document.removeEventListener('contextmenu', disableRightClick);
-    };
-  }, []);
+  //   // Cleanup the event listener on unmount
+  //   return () => {
+  //     document.removeEventListener('contextmenu', disableRightClick);
+  //   };
+  // }, []);
 
   return (
     <div className="flex flex-col min-h-screen">
