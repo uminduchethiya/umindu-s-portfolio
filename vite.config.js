55f0019722a588e6a9edef4fbc7328c44a6efc89
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2025-09-06 18:30:53 +0530

    updated one

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/umindu-s-portfolio/', // This should match your repository name
+  base: '/', // This should match your repository name
   build: {
     outDir: 'dist',
   },
